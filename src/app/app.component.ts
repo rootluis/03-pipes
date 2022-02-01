5cb08bf9d8b8aa882979f1b798da853c6964d110
Luis <rootluis85@gmail.com>
2022-01-31 20:59:31 -0600

    Generacion de Pipes
    Funcionalidad basica de pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  title        :  string = 'pipes';  
+  nombre       :  string = 'Capitán América';
+  nombre2      :  string = 'lUiS meJiA gaRciA';  
+  arreglo      :  string[] =['Capitán América', 'Spiderman', 'Batman', 'Ironman'];
+  PI           :  number = Math.PI;
+  porcentaje   :  number= 0.234;
+  salario      :  number=1234.5;
+  fecha        :  Date=new Date();
+  idioma       :  string='';
+  videoUrl     :  string='https://www.youtube.com/embed/JTDZ5O5lH_0';
+  activar      :  boolean =true;
+
+  valorPromesa = new Promise( (resolve) =>{
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  });
+
+  heroe={
+    nombre:'Spiderman',
+    casa:'Marvel',
+    edad: 17,
+    direccion:{
+      calle:'New York',
+      numero:100,
+      pais:'USA'
+    }
+
+  };
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Configuracion para incluir el idioma deceado
+import { registerLocaleData } from '@angular/common';
+import localEs from '@angular/common/locales/es';
+import localFr from '@angular/common/locales/fr';
+
+registerLocaleData(localEs);
+registerLocaleData(localFr);
+//Configuracion para incluir el idioma deceado
+
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+
+    //Configuracion para incluir el idioma deceado
+    {
+      provide: LOCALE_ID,
+      useValue: 'en'
+    }
+    //Configuracion para incluir el idioma deceado
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
